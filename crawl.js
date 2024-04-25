5f69df4df7b98b1c9b1c3bd6bea0ca3c7345cbc4
numeezbaloch17 <numeezbaloch17@gmail.com>
2024-04-25 10:46:29 +0530

    Crawled single page and handle the errors

--- crawl.js
+++ crawl.js
@@ -1,5 +1,30 @@
 const {JSDOM} = require('jsdom')
 
+async function crawlPage(currentURL){
+    console.log(`Actively crawling ${currentURL}`)
+    try{
+        const resp = await fetch(currentURL)
+        if(resp.status>399){
+            console.log(`Error in fetch with status code : ${resp.status} on page : ${currentURL}`)
+            return
+        }
+        const contentType = resp.headers.get("content-type")
+        if(contentType!="text/html"){
+            console.log(`Non html response : ${contentType} on page ${currentURL} `)
+            return
+        }
+
+        
+        console.log(await resp.text())
+
+    }catch(err){
+        console.log(`Error in fetch : ${err} while fetching ${currentURL}`)
+    }
+    
+   
+}
+
+
 function getUrlFromHTML(htmlBody,baseURL){
     urls=[]
     const dom = new JSDOM(htmlBody)
@@ -45,5 +70,6 @@ function normalizeUrl(urlString){
 
 module.exports={
     normalizeUrl,
-    getUrlFromHTML
+    getUrlFromHTML,
+    crawlPage,
 }
